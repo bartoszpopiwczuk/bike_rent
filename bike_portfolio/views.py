0774d1ac607e69a3813118dba52bb851c2cf3032
Bartosz Popiwczuk <bpopiwczuk@gmail.com>
2024-11-13 20:59:44 +0100

    Added rough draft of site using dummy data.

--- bike_portfolio/views.py
+++ bike_portfolio/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+bike_list = [
+    {
+        "brand": "Trek",
+        "model": "Slash 8",
+        "year": "2018",
+        "frame_size": "M",
+        "tire_size": '29"',
+    },
+    {
+        "brand": "Kellys",
+        "model": "Swag 10",
+        "year": "2019",
+        "frame_size": "M",
+        "tire_size": '27.5"',
+    },
+]
+
+
+def all_bikes(request):
+    context = {"website_title": "Choose your Bike", "bike_list": bike_list}
+    return render(request, "bike_portfolio/home.html", context)
