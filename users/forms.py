2a41f8b984230a7206e4ea9f0ff7a81977b5a54f
Bartosz Popiwczuk <bpopiwczuk@gmail.com>
2024-11-24 20:20:11 +0100

    Created user register form. Styled and working. Saves data to database.

---
+++ users/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+field_formatting = "w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-yellow-900 rounded border border-gray-600 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+
+
+class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": field_formatting,
+                "placeholder": "First Name",
+            }
+        ),
+    )
+    last_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": field_formatting,
+                "placeholder": "Last Name",
+            }
+        ),
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": field_formatting,
+                "placeholder": "Email",
+            }
+        )
+    )
+    password1 = forms.CharField(
+        max_length=100,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": field_formatting,
+                "placeholder": "Password",
+            }
+        ),
+    )
+    password2 = forms.CharField(
+        max_length=100,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": field_formatting,
+                "placeholder": "Repeat Password",
+            }
+        ),
+    )
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email", "password1", "password2"]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import (
-    UserCreationForm,
-)  # gotowy formularz do tworzenia użytkowników
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def register(request):
-    context = {'form': UserCreationForm()}
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created")
+            return redirect("all-bikes")
+    else:
+        form = UserRegisterForm()
+    context = {"form": form}
     return render(request, "users/register.html", context)
