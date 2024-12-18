28b06f5f1a8add24599e833ee3d75411bac62f93
Bartosz Popiwczuk <bpopiwczuk@gmail.com>
2024-12-18 17:08:04 +0100

    Biike purpose categories filtering in the header

--- bike_portfolio/urls.py
+++ bike_portfolio/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.all_bikes, name="all-bikes"),
     path("bike-detail/<int:pk>", views.bike_detail, name="bike-detail"),
+    path("purpose/<str:purpose>", views.bike_purpose, name="bike-purpose"),
 ]
--- bike_portfolio/views.py
+++ bike_portfolio/views.py
@@ -13,3 +13,22 @@ def bike_detail(request, pk):
         "bike": Bicycle.objects.get(id=pk),  # TODO: fliter by id
     }
     return render(request, "bike_portfolio/bike_detail.html", context)
+
+
+def bike_purpose(request, purpose):
+    if purpose == "mtb":
+        web_title = "Mountain Bikes"
+    elif purpose == "road":
+        web_title = "Road Bikes "
+    elif purpose == "gravel":
+        web_title = "Gravel Bikes"
+    elif purpose == "city":
+        web_title = "City Bikes"
+    else:
+        web_title = "Other Bikes"
+
+    context = {
+        "website_title": web_title,
+        "bike_list": Bicycle.objects.filter(purpose=purpose),
+    }
+    return render(request, "bike_portfolio/home.html", context)
