c07fca40a22bf388caead912df045abaf92d49ec
Bartosz Popiwczuk <bpopiwczuk@gmail.com>
2025-03-03 19:08:00 +0100

    Separated header, footer, search from base.html for better compartmentalization

--- bike_portfolio/utils.py
+++ bike_portfolio/utils.py
@@ -5,15 +5,16 @@ from .models import Bicycle
 
 
 def paginateBicycles(request, objects, objects_per_page) -> tuple:
+
     paginator = Paginator(objects, per_page=objects_per_page)
     page_number = request.GET.get("page")
-    bikes: Page = paginator.get_page(page_number)
+    bikes: Page = paginator.get_page(number=page_number)
 
     return bikes, paginator
 
 
 def searchBicycles(request) -> tuple:
-    search_query: str = ""
+    search_query: str = "" # initial search query is going to be empty, empty string gives all objects
     if request.GET.get("search_query"):
         search_query = request.GET.get("search_query")
     objects = Bicycle.objects.filter(
