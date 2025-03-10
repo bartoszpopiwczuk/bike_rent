e132b6ea2a3e12510388ad690f9bb2fc13cb07ad
Bartosz Popiwczuk <bpopiwczuk@gmail.com>
2025-03-10 19:50:45 +0100

    Created API

---
+++ api/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+
+
+def getRoutes(request):
+    routes = [
+        {'GET': '/api/bikes'},
+        {'GET': '/api/bikes/id'},
+        {'POST': '/api/bikes/id/review'},
+
+        {'POST': '/api/bikes/token'},
+        {'POST': '/api/bikes/token/refresh'},
+    ]
+    return JsonResponse(routes)
